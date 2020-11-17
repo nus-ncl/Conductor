2a309bc2938b6ea04056626646c10a59e8db8302
hkwany <380868513@qq.com>
2020-11-17 21:31:11 +0800

    docker update

--- bin/renderer_test.py
+++ bin/renderer_test.py
@@ -6,6 +6,7 @@ import jinja2
 from jinja2 import Environment, PackageLoader
 import default
 import yaml_parser
+import pathlib
 
 
 def vagrantfile_renderer(vms):
@@ -90,6 +91,7 @@ def clientfile_renderer(experiment_metadata, vms):
 
 
 def dockerfile_renderer(vms):
+	header=0
 	services={}
 	ROOT_DIR = default.conductor_path
 	template_dir = [f"{default.conductor_path}/templates", f"{default.conductor_path}/services"]
@@ -97,32 +99,55 @@ def dockerfile_renderer(vms):
 	env = Environment(loader=loader)
 	# output Dockerfile
 	for vm in vms:
-		if vm['services'] and vm['os']:
-			with open(f"{default.DOCKERFILE_TEST_FILE}_{vm['hostname']}_{vm['services'][0]['name']}", 'w') as file:
-				# get docker template file of specific service
-				dockerfile = env.get_template(f"{vm['services'][0]['name']}/{vm['services'][0]['name']}_docker.j2")
-				# load specification(YAML-style) file of specific service
-				with open(f"{default.conductor_path}/services/{vm['services'][0]['name']}/{vm['services'][0]['name']}.yml", 'r') as stream:
-					service = yaml.safe_load(stream)
-					services[f"{vm['services'][0]['name']}"]=copy.deepcopy(service)
-				# print('service->')
-				# print(service)
-				content = dockerfile.render(vm=vm, service=service)
-				# content = ansiblefile.render(vm=vm, USER='{{ ansible_env.USER }}', conductor_path=ROOT_DIR, service=service)
-				file.write(content)
+		if vm['provider'] == 'docker':
+			if vm['services'] and vm['os']:
+				if not header:
+					try:
+						with open(default.DOCKERCOMPOSE_TEST_FILE, 'w') as file:
+							file.write("version: '3.8'\nservices:\n")
+					except OSError:
+						print(f"{default.DOCKERCOMPOSE_TEST_FILE} open failure")
+					header=1
+				# one docker one service, so here specify vm['services'][0], cuz vm['services'] is 1-element list
+				with open(f"{default.DOCKERFILE_TEST_FILE}_{vm['hostname']}_{vm['services'][0]['name']}", 'w') as file:
+					# get docker template file of specific service
+					dockerfile = env.get_template(f"{vm['services'][0]['name']}/{vm['services'][0]['name']}_docker.j2")
+					# load specification(YAML-style) file of specific service
+					try:
+						with open(f"{default.conductor_path}/services/{vm['services'][0]['name']}/{vm['services'][0]['name']}.yml", 'r') as stream:
+							service = yaml.safe_load(stream)
+							services[f"{vm['services'][0]['name']}"]=copy.deepcopy(service)
+						# print('service->')
+						# print(service)
+						content = dockerfile.render(vm=vm, service=service)
+						# content = ansiblefile.render(vm=vm, USER='{{ ansible_env.USER }}', conductor_path=ROOT_DIR, service=service)
+						file.write(content)
+						print(f"{default.DOCKERFILE_TEST_FILE}_{vm['hostname']}_{vm['services'][0]['name']} is generated!")
+					except OSError:
+						print('No Such Service for Docker')
+
+					# print('services->')
+					# print(services)
+					# print(vm['services'][0]['name'])
+					# print(vm['hostname'])
+					# output docker-compose.yml
+					try:
+						with open(default.DOCKERCOMPOSE_TEST_FILE, 'a') as file:
+							docker_compose_file = env.get_template('docker-compose.j2')
+							content = docker_compose_file.render(vm=vm, services=services, conductor_path=ROOT_DIR)
+							file.write(content)
+							print(f"{default.DOCKERCOMPOSE_TEST_FILE} is updated! ")
+					except OSError:
+						print(f"{default.DOCKERCOMPOSE_TEST_FILE} open error")
+			else:
+				print('No Any Service or OS to generate This Dockerfile of a container!')
 		else:
-			print('No Any Service or OS to generate This Dockerfile of a container!')
-	print('services->')
-	print(services)
-	# output docker-compose.yml
-	with open(default.DOCKERCOMPOSE_TEST_FILE, 'w') as file:
-		docker_compose_file = env.get_template('docker-compose.j2')
-		content = docker_compose_file.render(vms=vms,services=services,conductor_path=ROOT_DIR)
-		file.write(content)
+			# This is no docker
+			pass
 
 if __name__ == "__main__":
 	yaml_content = yaml_parser.yaml_file_load('output')
-	print(yaml_content)
+	# print(yaml_content)
 	vms, lans, nodes, docker_networks = yaml_parser.yaml_content_parser(yaml_content)
 	if default.debug:
 		# print('nodes->')
@@ -131,11 +156,11 @@ if __name__ == "__main__":
 		# print(lans)
 		# print('docker_networks->')
 		# print(docker_networks)
-		print('vms->')
-		print(vms)
+		# print('vms->')
+		# print(vms)
 	# vagrantfile_renderer(vms)
 	# hosts_renderer(vms)
 	# nodesfile_renderer(nodes,default.NODE_VIRTUALBOX_VERSION)
 	# NSfile_renderer(lans, nodes)
 	# ansiblefile_renderer(vms)
-	dockerfile_renderer(vms)
+		dockerfile_renderer(vms)
